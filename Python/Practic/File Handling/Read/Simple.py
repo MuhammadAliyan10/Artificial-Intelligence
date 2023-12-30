44f759e2669f166ae76afa197e0f0dcf728b9342
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-12-30 20:20:41 +0500

    Updated and Added

---
+++ Python/Practic/File Handling/Read/Simple.py
@@ -0,0 +1,4 @@
+file_path = 'Sample.txt'
+with open(file_path, 'r') as f:
+    content = f.read()
+    print(content)
---
+++ Python/Practic/File Handling/Write/Simple.py
@@ -0,0 +1,5 @@
+file_path ='example.txt'
+with open(file_path, 'a') as f:
+    f.write("How are you? \n")
+    f.write("Bro war go")
+    
